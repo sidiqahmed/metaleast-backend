67c743ee2137f32e0af95ad623b160125aac722e
jeromemeichelbeck <jerome@meichelbeck.io>
2019-01-01 20:12:51 +0100

    minor refactor of updateUser resolver

--- src/resolvers/Mutation/updateUser.js
+++ src/resolvers/Mutation/updateUser.js
@@ -2,6 +2,8 @@ import bcrypt from 'bcryptjs'
 
 import getUserId from '../../utils/getUserId'
 import validatePassword from '../../utils/validatePassword'
+import isAdmin from '../../utils/isAdmin'
+import isSuperadmin from '../../utils/isSuperadmin'
 
 const updateUser = async (
   parent,
@@ -9,6 +11,9 @@ const updateUser = async (
   { prisma, request },
   info
 ) => {
+  if (!data) {
+    data = {}
+  }
   // Get user ID from token (will throw an error if unauthenticated)
   const userId = getUserId(request)
   const connectedUser = await prisma.query.user({ where: { id: userId } })
@@ -37,13 +42,25 @@ const updateUser = async (
 
   // If id is given and connected user is admin, update given user
   const userToUpdate = await prisma.query.user({ where: { id } })
-  const isAdmin = ['ADMIN', 'SUPERADMIN'].indexOf(connectedUser.role) > -1
-  if (isAdmin) {
+  if (isAdmin(connectedUser)) {
     // Admin can't update user password
 
-    // Admin can't give or take away admin rights
-    if (connectedUser.role === 'ADMIN') {
+    // Nobody can change role to superadmin
+    if (isSuperadmin(data)) {
       data.role = userToUpdate.role
+    }
+
+    if (!isSuperadmin(connectedUser)) {
+      // Admin (not superadmin) can't give or take away admin rights
+      if (isAdmin(userToUpdate) || isAdmin(data)) {
+        data.role = userToUpdate.role
+      }
+      // Admin can't update admin or superadmin
+      if (isAdmin(userToUpdate)) {
+        data.name = userToUpdate.name
+        data.email = userToUpdate.email
+        data.role = userToUpdate.role
+      }
     }
 
     return prisma.mutation.updateUser({ where: { id }, data }, info)
