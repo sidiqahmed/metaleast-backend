bed8550cada06455eb4b9c1d4ec98f801a833a8a
jeromemeichelbeck <jerome@meichelbeck.io>
2019-01-01 14:44:22 +0100

    updated login resolver: now a user disabled or banned can't connect

--- src/resolvers/Mutation/login.js
+++ src/resolvers/Mutation/login.js
@@ -2,26 +2,60 @@ import bcrypt from 'bcryptjs'
 
 import generateToken from '../../utils/generateToken'
 
-const login = async (parent, { data }, { prisma }, info) => {
-  const user = await prisma.query.user({
-    where: {
-      email: data.email
+const login = {
+  async resolve(parent, { data }, { prisma }, info) {
+    const user = await prisma.query.user(
+      { where: { email: data.email } },
+      `
+      {
+        id
+        password
+        enabled
+        banned {
+          id
+          comment
+          debanned
+        }
+      }
+    `
+    )
+
+    console.log(user)
+    // Check if user exists
+    if (!user) {
+      throw new Error('Unable to login')
     }
-  })
 
-  if (!user) {
-    throw new Error('Unable to login')
-  }
+    // Check password
+    const isMatch = await bcrypt.compare(data.password, user.password)
 
-  const isMatch = await bcrypt.compare(data.password, user.password)
+    if (!isMatch) {
+      throw new Error('Unable to login')
+    }
 
-  if (!isMatch) {
-    throw new Error('Unable to login')
-  }
+    // Check if enabled
+    if (!user.enabled) {
+      throw new Error('User is not enabled')
+    }
 
-  return {
-    user,
-    token: generateToken(user.id)
+    // Check if banned
+    if (user.banned !== null) {
+      const debanned = new Date(user.banned.debanned)
+      const now = new Date()
+
+      // Check if banned expired
+      if (debanned < now) {
+        // If debanned, remove from database
+        await prisma.mutation.deleteBanned({ where: { id: user.banned.id } })
+      } else {
+        throw new Error('User is banned')
+      }
+    }
+
+    return {
+      user,
+      token: generateToken(user.id)
+    }
   }
 }
 
